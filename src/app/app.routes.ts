8d36ade890ca665b7b3cf014dfb1e1b2c0d02ae5
appie999 <aitoutmghartfarid@gmail.com>
2025-04-11 23:11:28 +0100

    create cv component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import {AccueilComponent, accueilComponent} from "./accueil/accueil.component";
 import {AproposComponent, aproposComponent} from "./apropos/apropos.component";
 import {ContactComponent, contactComponent} from "./contact/contact.component";
 import {ProjetsComponent, projetsComponent} from "./projets/projets.component";
+import {CvComponent} from "./cv/cv.component";
 
 export const routes: Routes = [
+  {path : "", component : AccueilComponent},
   {path : "accueil", component : AccueilComponent},
   {path : "apropos", component : AproposComponent},
   {path : "contact", component : ContactComponent},
   {path : "projets", component : ProjetsComponent},
+  {path : "cv", component : CvComponent},
 ];
